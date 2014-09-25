7a94544e3dbd095ba961ee03d983fbfed2972f9e
dubenecbv <dubenecbv@gmail.com>
2014-09-25 23:48:23 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>Официальное приложение Sofino.ua</title>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+<link href="http://sofino.ua/apk/css/style.css" rel="stylesheet" />
+</head>
+<body>
+
+<?php
+
+echo '<h2>Hello is work! Все работает.</h2>';
+
+?>
+
+<script type="text/javascript" src="http://sofino.ua/apk/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="http://sofino.ua/apk/js/scripts.js"></script>
+
+</body>
+</html>
